fc68651dd1c0258cf4ec7263c8ef2a8e07afd8d5
Bekdaulet <4bekdaulet@gmail.com>
2021-04-25 22:54:42 +0600

    Add logic

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Бекдаулет Касымов on 4/25/21.
+//  Copyright © 2021 Бекдаулет Касымов. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+    let name = "SwiftyLib"
+
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
